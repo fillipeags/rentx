34a4ea5940b3ecd11fd1fa33cf083801d85b823b
Fillipe <fillipe484@gmail.com>
2021-09-10 13:40:00 -0300

    feat: implement react-native-calendars

--- src/screens/Schedules/index.tsx
+++ src/screens/Schedules/index.tsx
@@ -18,6 +18,7 @@ import {
 } from './styles';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 export function Schedules() {
   const theme = useTheme()
@@ -57,7 +58,7 @@ export function Schedules() {
       </Header>
 
       <Content>
-
+        <Calendar />
       </Content>
 
       <Footer>
