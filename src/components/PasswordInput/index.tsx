408646620bbcb77c70293a4c1e37536ecffcb3a6
Fillipe <fillipe484@gmail.com>
2021-09-25 21:38:06 -0300

    feat: accessibility inputs style

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -11,14 +11,18 @@ import {
 import { TextInputProps } from 'react-native';
 
 interface Props extends TextInputProps {
-  iconName: React.ComponentProps<typeof Feather>['name']
+  iconName: React.ComponentProps<typeof Feather>['name'],
+  value?: string
 }
 
 export function PasswordInput({
   iconName,
+  value,
   ...rest
 }: Props) {
   const [isPasswordVisible, setIsPasswordVisibile] = useState(true);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
   const theme = useTheme()
 
@@ -26,19 +30,30 @@ export function PasswordInput({
     setIsPasswordVisibile(prevState => !prevState)
   }
 
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value)
+  }
+
   return (
-    <Container>
+    <Container isFocused={isFocused}>
       <IconContainer>
         <Feather
           name={iconName}
           size={24}
-          color={theme.colors.text_detail}
+          color={(isFocused || isFilled) ? theme.colors.main : theme.colors.text_detail}
         />
       </IconContainer>
 
       <InputText
-        {...rest}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         secureTextEntry={isPasswordVisible}
+        {...rest}
       />
 
       <BorderlessButton onPress={handlePasswordVisibilityChange}>
